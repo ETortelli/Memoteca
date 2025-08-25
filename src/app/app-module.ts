a7a7b649430885609b5c3d31334aff19ca0f499e
Eduardo Tortelli <eduardo.tortelli@ndd.tech>
2025-08-25 11:39:40 -0300

    implementado rodape

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Home } from './home/home';
 import { Cabecalho } from './componentes/cabecalho/cabecalho';
+import { Rodape } from './componentes/rodape/rodape';
 
 @NgModule({
   declarations: [
     App,
     Home,
-    Cabecalho
+    Cabecalho,
+    Rodape
   ],
   imports: [
     BrowserModule,
