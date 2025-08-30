744a394112ae0bcee77d3828f9ed67d7a8b9360f
Eduardo Tortelli <eduardo.tortelli@ndd.tech>
2025-08-30 20:12:40 -0300

    criado pensamentos e aplicados diretivas de componentes e diretivas angular

---
+++ src/app/componentes/pensamentos/pensamento/pensamento.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  standalone: false,
+  templateUrl: './pensamento.html',
+  styleUrl: './pensamento.css'
+})
+export class Pensamento {
+
+  @Input() pensamento = {
+    conteudo: 'I love Angular',
+    autoria: 'Eduardo Tortelli',
+    modelo: 'modelo3'
+  }
+
+  larguraPensamento(): string{
+    if(this.pensamento.conteudo.length >= 256){
+      return 'pensamento-g';
+    }
+    
+    return 'pensamento-p';
+  }
+
+}
