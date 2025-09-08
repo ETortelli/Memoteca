711b3bd48c4a0e76aca42ea6e7094c9bacb331d9
Eduardo Tortelli <eduardo.tortelli@ndd.tech>
2025-09-08 10:07:51 -0300

    implementado começo do formulario reativo

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -7,7 +7,7 @@ import { Home } from './home/home';
 import { Cabecalho } from './componentes/cabecalho/cabecalho';
 import { Rodape } from './componentes/rodape/rodape';
 import { CriarPensamento } from './componentes/pensamentos/criar-pensamento/criar-pensamento';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ListarPensamento } from './componentes/pensamentos/listar-pensamento/listar-pensamento';
 import { Pensamento } from './componentes/pensamentos/pensamento/pensamento';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
@@ -31,6 +31,7 @@ import { EditarPensamento } from './componentes/pensamentos/editar-pensamento/ed
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PensamentoModel } from '../pensamento.model';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -11,22 +12,24 @@ import { Router } from '@angular/router';
 })
 export class CriarPensamento implements OnInit {
 
-  pensamento: PensamentoModel = {
-    conteudo: '',
-    autoria: '',
-    modelo: 'modelo1',
-  }
+  formulario!: FormGroup
 
   constructor(
     private servicePensamento: PensamentoService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: ['Formulário reativo'],
+      autoria: ['Angular'],
+      modelo: ['modelo1']
+    });
   }
 
   criarPensamento() {
-    this.servicePensamento.criar(this.pensamento).subscribe(() => { });
+    this.servicePensamento.criar(this.formulario.value).subscribe(() => { });
     this.router.navigate(['/listarPensamento']);
   }
 
